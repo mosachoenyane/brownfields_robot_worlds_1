fa22c39311b9978774edf8108a452b5e5928705e
Mmeli Ntshakala <mmeliroot@gmail.com>
2025-07-15 06:02:16 +0200

    field declarations(test)

--- src/test/java/za/co/wethinkcode/Acceptance2/Look/LookCommandTest.java
+++ src/test/java/za/co/wethinkcode/Acceptance2/Look/LookCommandTest.java
@@ -1,4 +1,14 @@
 package za.co.wethinkcode.Acceptance2.Look;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import za.co.wethinkcode.client.RobotWorldClient;
+import za.co.wethinkcode.client.RobotWorldJsonClient;
+import za.co.wethinkcode.server.model.Position;
+
 public class LookCommandTest {
+    private final static int defaultPort = 5000;
+    private final static String defaultIP = "localhost";
+    private final static int worldSize = 2;
+    private final static Position obstacle = new Position(2,1);
+    private final RobotWorldClient serverClient = new RobotWorldJsonClient();
 }
