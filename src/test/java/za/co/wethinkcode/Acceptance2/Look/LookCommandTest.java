fc82f028facd65710d9ee4a5faa3e4e9378e0c01
Mmeli Ntshakala <mmeliroot@gmail.com>
2025-07-15 06:24:21 +0200

    Launched response Look(test)

--- src/test/java/za/co/wethinkcode/Acceptance2/Look/LookCommandTest.java
+++ src/test/java/za/co/wethinkcode/Acceptance2/Look/LookCommandTest.java
@@ -3,9 +3,12 @@ package za.co.wethinkcode.Acceptance2.Look;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import za.co.wethinkcode.client.RobotWorldClient;
 import za.co.wethinkcode.client.RobotWorldJsonClient;
 import za.co.wethinkcode.server.model.Position;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LookCommandTest {
     private final static int defaultPort = 5000;
@@ -27,5 +30,17 @@ public class LookCommandTest {
      */
     @AfterEach void disconnectTheServer(){
         serverClient.disconnect();
+    }
+
+    @Test void lookRobotWithObstacle(){
+        assertTrue(serverClient.isConnected());
+        String launchRequest = "{" +
+                "  \"robot\": \"HAL\"," +
+                "  \"command\": \"launch\"," +
+                "  \"arguments\": [\"shooter\",\"5\",\"5\"]" +
+                "}";
+        JsonNode launchResponse = serverClient.sendRequest(launchRequest);
+        assertNotNull(launchResponse);
+
     }
 }
